76d4e833d1a76c225c0594d6de5dd0a86ef01e98
soetanto <c.soetantoaa@gmail.com>
2022-05-15 23:31:06 +0700

    fix(question one): change channel to responses

--- src/handler/message_handler.go
+++ src/handler/message_handler.go
@@ -47,7 +47,7 @@ func MessageCreateHandlerQuestionOne(s *discordgo.Session, m *discordgo.MessageC
 			log.Println(err)
 			return
 		}
-		_, err = s.ChannelMessageSend(util.VettingQuestioningChannelId, fmt.Sprintf("Hey <@%s>! It looks like you missed question 1. Please re-read the <#%s> again, we assure you that the code is in there. Thank you for your understanding.\nPS: if you are sure you got it right, please ignore this message.", userId, util.RulesVettingChannelId))
+		_, err = s.ChannelMessageSend(util.ResponsesChannelId, fmt.Sprintf("Hey <@%s>! It looks like you missed question 1. Please re-read the <#%s> again, we assure you that the code is in there. Thank you for your understanding.\nPS: if you are sure you got it right, please ignore this message.", userId, util.RulesVettingChannelId))
 		if err != nil {
 			log.Println(err)
 			return
