281ba67e8e5c2c31664114c472e7ae211b6f1d75
soetanto <c.soetantoaa@gmail.com>
2022-05-18 07:59:20 +0700

    chore(graceful shutdown): remove SIGTERM from channel notify to maybe prevent heroku restart

--- src/main.go
+++ src/main.go
@@ -70,7 +70,8 @@ func main() {
 	// Wait here until CTRL-C or other term signal is received.
 	fmt.Println("Bot is now running.  Press CTRL-C to exit.")
 	sc := make(chan os.Signal, 1)
-	signal.Notify(sc, syscall.SIGINT, syscall.SIGTERM, os.Interrupt, os.Kill)
+	//syscall.SIGTERM,
+	signal.Notify(sc, syscall.SIGINT, os.Interrupt, os.Kill)
 	<-sc
 
 	if *RemoveCommands {
