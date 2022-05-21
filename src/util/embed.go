1c8e4e9554866e5e34acf737d9bc6a138d6b3690
soetanto <c.soetantoaa@gmail.com>
2022-05-21 19:51:46 +0700

    change welcome image to SFW (lol) wojak

--- src/util/embed.go
+++ src/util/embed.go
@@ -9,7 +9,7 @@ const (
 	WelcomeTitle        = "Welcome to Servus Dei!"
 	LogoURL             = "https://cdn.discordapp.com/avatars/767426889294938112/0e100e9fec18866892ed0c875b341926.png"
 	Author              = "Servant of Servus Dei"
-	WelcomeImageURL     = "https://media.discordapp.net/attachments/751174184733900801/974282451096576041/unknown.png"
+	WelcomeImageURL     = "https://cdn.discordapp.com/attachments/751174152588623912/976921809607880714/You_Doodle_2022-05-19T18_58_15Z.jpg"
 	WelcomeImage2URL    = "https://media.discordapp.net/attachments/751174152588623912/975368929008558130/Screenshot_2022-05-11_at_11.42.51_PM.png"
 	FooterText          = "2022 | Made for Servus Dei by soetanto™\u3000\u3000\u3000\u3000\u3000\u3000\u3000\u3000\u3000\u3000\u3000\u3000\u3000\u3000\u3000\u3000\u3000\u3000\u3000\u3000\u3000\u3000\u3000"
 	ServusDeiWebsiteURL = "https://www.servusdeicatholic.com/"
